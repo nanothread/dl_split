08e749903a8262f90871b53eb0464a5a469b0b2e
Andrew Glen <andrew.r.glen@gmail.com>
2020-03-26 21:35:32 +0000

    Add code to split up timestamp and title

--- timestamp.py
+++ timestamp.py
@@ -1,3 +1,5 @@
+import re
+
 def crawl_timestamps(text):
 	'''
 		Returns a dictionary of {int: string} where int is the
@@ -5,9 +7,50 @@ def crawl_timestamps(text):
 		the string is the name of the song.
 	'''
 	
+#	for line in text.split("\n"):
+#		print("Testing " + line)
+#		print(" -> " + str(re.search(r"[0-9]:[0-9][0-9]", line)))
 	
+	lines = text.split("\n")
+	lines = list(filter(lambda line: not re.search(r"[0-9]:[0-9][0-9]", line) == None, lines))
+	lines = list(map(lambda line: line.strip(), lines))
+	lines = list(map(lambda line: getTitleAndTimestamp(line), lines))
+	return lines
+	
+def getTitleAndTimestamp(text):
+	search = getTimeSearch(text)
+	
+	if search != None:
+		time = text[search.span()[0] : search.span()[1]]
+		
+		remainder = text[search.span()[1] :]
+		nameSearch = re.findall(r"[:alpha:]", remainder) # [^([:digit:]|[:punct:]|[:space:])]
+		if nameSearch == None:
+			return (time, remainder)
+		
+		name = remainder[nameSearch[0].span()[0] :]
+		return(time, name)
+		
+def getIndexOfFirstAlphaChar(text):
+	for i in range(len(text)):
+		c = ord(text[i])
+		if ord(A) <= c <= ord(Z) or ord('a') <= c <= ord('z'):
+			return i
+	
+def getTimeSearch(text):
+	hourSearch = re.search(r"[0-9]?[0-9]:[0-9][0-9]:[0-9][0-9]", text)
+	if hourSearch != None:
+		return hourSearch
+	
+	return re.search(r"[0-9]?[0-9]:[0-9][0-9]", text)
+		
 
 if __name__ == "__main__":
+	print(ord('a'))
+	print(ord('z'))
+	print(ord('A'))
+	print(ord('Z'))
+	print(chr(94))
 	# TODO: Testing
 	print(crawl_timestamps('''
 	A trip into the future. 
@@ -25,4 +68,5 @@ if __name__ == "__main__":
 	24:31 A.L.I.S.O.N & Rosentwig - Short Circuit
 	29:11 ＹＯＵＴＨ ８３ - Euphoria
 	33:37 Memorex Memories - Thanks for listening
+	01:11:02 A Random Song
 	'''))
