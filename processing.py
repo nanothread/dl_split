d14bb30594055da84c7dcadbce5496ee63fb8613
Andrew Glen <andrew.r.glen@gmail.com>
2020-03-27 01:16:09 +0000

    Better progress updating

--- processing.py
+++ processing.py
@@ -4,17 +4,17 @@ import ffmpeg
 from timestamp import crawl_timestamps
 import os 
 
-def splitAudioFile(filename, timestamps):
-	if not os.path.exists("./tracks"):
-		os.makedirs("./tracks")
+def splitAudioFile(filename, timestamps, outputFolder):
+	if not os.path.exists(outputFolder):
+		os.makedirs(outputFolder)
 		
 	numTracks = len(timestamps)
 	for i in range(numTracks):
 		startTime, name = timestamps[i]
 		endTime = timestamps[i + 1][0] if i + 1 < len(timestamps) else None
 		
-		print(f"Making Track ({i}/{numTracks}): {name};")
-		trimOne(filename, f"./tracks/{name}.m4a", startTime, endTime)
+		print(f" - Making Track ({i + 1}/{numTracks}): {name}")
+		trimOne(filename, f"{outputFolder}/{name}.m4a", startTime, endTime)
 		
 def trimOne(inputFile, outputFile, startTime, endTime):
 	inputArgs = {
@@ -33,5 +33,4 @@ if __name__ == "__main__":
 	with open("ASTRO - A Synthwave Mix [Chillwave - Retrowave - Synthwave]-XccPsuqAz4E.description", "r") as f:
 		times = crawl_timestamps(f.read())
 		
-#	trimOne("ASTRO - A Synthwave Mix [Chillwave - Retrowave - Synthwave]-XccPsuqAz4E.m4a", "out.m4a", 0, 65)
 	splitAudioFile("ASTRO - A Synthwave Mix [Chillwave - Retrowave - Synthwave]-XccPsuqAz4E.m4a", times)
